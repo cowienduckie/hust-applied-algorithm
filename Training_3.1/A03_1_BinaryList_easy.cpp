9bf30f1d5e38d0fe4d10dc73a237ac745eb77c8f
Minh Tran <minhtran.hust3801@gmail.com>
2021-03-15 00:12:49 +0700

    Update A03_1

---
+++ Training_3.1/A03_1_BinaryList_easy.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+     
+using namespace std;
+         
+int num = 0;
+
+void printRes(int binary){
+    for (int i = num - 1; i >= 0; --i){
+        
+        cout << (int)(binary >> i & 1);
+    }
+    cout << endl;
+}
+
+void binaryList(int pos, int binary){
+    if (pos == num){
+        printRes(binary);
+
+        return;
+    }
+
+    binary = binary << 1;   binaryList(pos + 1, binary);
+
+    binary = binary + 1;  binaryList(pos + 1, binary);
+}
+
+     
+int main(int argc, char **argv){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin >> num;
+     
+    binaryList(0, 1);
+
+    return 0;
+}
